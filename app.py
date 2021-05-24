71ba0f38fe97510c4fd073ba2910fff67ea23c57
noeldolores <noel.dolore@gmail.com>
2021-05-23 20:52:09 -0700

    fixed issue with removing the last favorite in the list left a blank line

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import crypto_lookup
 app = Flask(__name__)
 app.config.from_object("config.DevelopmentConfig")
 
-FAVORITES = Path("favorites.txt")
+FAVORITES = "favorites.txt"
 
 def run_search(coin_query):
   coin = crypto_lookup.Query(coin_query)
@@ -24,13 +24,12 @@ def run_search(coin_query):
 
 def reorder_list(correct_order=list, thread_output=list):
   corrected_list = []
-
   for i in range(len(correct_order)):
     coin_check = correct_order[i].lower()
     for coin in thread_output:
       if coin['name'].lower() == coin_check or coin['symbol'].lower() == coin_check or str(coin['id']).lower() == coin_check:
         corrected_list.append(coin)
-
+        
   return corrected_list
 
 
@@ -46,21 +45,23 @@ def search():
   in_favorites = False
 
   if request.method == 'POST':
-    if request.form['coin_query'] != "":
-      session.pop('result', None)
-      coin_query = request.form['coin_query']
-      result, bad_query = run_search(coin_query)
+    print(request.form)
+    if 'coin_query' in request.form:
+      if request.form['coin_query'] != "":
+        session.pop('result', None)
+        coin_query = request.form['coin_query']
+        result, bad_query = run_search(coin_query)
 
-      with open('favorites.txt', 'r') as f:
-        temp_list = f.read().lower().splitlines()
-        if result['name'].lower() in temp_list or result['symbol'].lower() in temp_list:
-          in_favorites = True
+        with open(FAVORITES, 'r') as f:
+          favorites = f.read().lower().splitlines()
+          if str(result['name']).lower() in favorites or str(result['symbol']).lower() in favorites:
+            in_favorites = True
 
     elif "add_favorites" in request.form:
       to_add = request.form['add_favorites']
-      with open('favorites.txt', 'a') as f:
+      with open(FAVORITES, 'a') as f:
         if to_add is not None:
-          if FAVORITES.stat().st_size > 0:
+          if Path(FAVORITES).stat().st_size > 0:
             f.write('\n' + to_add)
           else:
             f.write(to_add)
@@ -70,8 +71,6 @@ def search():
       result = session['result']
       bad_query = session['bad_query']
       session.pop('result', None)
-
-
   return render_template('search.html', result=result, bad_query=bad_query, in_favorites=in_favorites)
 
 
@@ -79,29 +78,29 @@ def search():
 def favorites():
   if request.method == 'POST':
     if 'remove_favorite' in request.form:
-      with open('favorites.txt', 'r') as f:
-        temp_list = f.read().splitlines()
-      with open('favorites.txt', 'w') as f:
-        for coin in temp_list:
-          if coin.lower() not in request.form['remove_favorite'].split(','):
-            if temp_list[-1] == coin:
-              f.write(coin)
-            else:
-              f.write(coin + '\n')
+      to_remove = request.form['remove_favorite'].lower().split(',')
 
+      with open(FAVORITES, 'r') as f:
+        temp_list = f.read().lower().splitlines()
+
+      with open(FAVORITES, 'w') as f:
+        index_check = 2
+        for i in range(len(temp_list)):
+          if temp_list[i] not in to_remove:
+            if i == len(temp_list) - index_check:
+              f.write(temp_list[i])
+            else:
+              f.write(temp_list[i] + '\n')
       return redirect(url_for("favorites"))
 
     elif 'coin_query' in request.form:
       coin_query = request.form['coin_query']
       session['result'], session['bad_query'] = run_search(coin_query)
-
       return redirect(url_for("search"))
 
-
   elif request.method == 'GET':
-    favorites_file = Path("favorites.txt")
-    if favorites_file.is_file() and favorites_file.stat().st_size > 0:
-      with open(favorites_file, 'r') as f:
+    if Path(FAVORITES).is_file() and Path(FAVORITES).stat().st_size > 0:
+      with open(FAVORITES, 'r') as f:
         favorite_coins = f.read().splitlines()
 
         for i in range(len(favorite_coins)):
@@ -111,12 +110,11 @@ def favorites():
           process = Thread(target=crypto_lookup.Query, args=[favorite_coins[i], thread_output])
           process.start()
           threads.append(process)
-
         for process in threads:
           process.join()
 
         corrected_list = reorder_list(favorite_coins, thread_output)
-
       return render_template('favorites.html', favorites_list=corrected_list)
+
     else:
       return redirect(url_for("search"))
